21af8e8bdd9053d8c44144305b4dbf7e622944ab
Silius Xix <65783933+SiliusJM@users.noreply.github.com>
2025-03-20 13:06:53 -0500

    Crud

---
+++ Data/BlazorAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp.Models;
+
+namespace BlazorApp.Data
+{
+    public class BlazorAppContext : DbContext
+    {
+        public BlazorAppContext (DbContextOptions<BlazorAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El título es requerido.")] // Campo requerido
+        [StringLength(100, ErrorMessage = "El título no puede tener más de 100 caracteres.")] // Longitud máxima
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "La fecha de publicación es requerida.")] // Campo requerido
+        public DateOnly ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "El género es requerido.")] // Campo requerido
+        public string? Genre { get; set; }
+
+        [Required(ErrorMessage = "El precio es requerido.")] // Campo requerido
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0.")] // Rango válido
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
